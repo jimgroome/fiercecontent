e9faac6b3bd205b5bb7156c35585dd503cb009f6
Jim Groome <jim.groome@marketingdelivery.com>
2019-07-02 10:47:56 +0100

    Updates

---
+++ precache-manifest.b77401591c9667559df56528865c992d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8d2b18760e3f803c1bdbe648a4515a",
+    "url": "/fiercecontent/index.html"
+  },
+  {
+    "revision": "7e8b46570936b5e5925a",
+    "url": "/fiercecontent/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/fiercecontent/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7e8b46570936b5e5925a",
+    "url": "/fiercecontent/static/js/main.6d87adf7.chunk.js"
+  },
+  {
+    "revision": "01dbd3222ad7af5b012d",
+    "url": "/fiercecontent/static/js/runtime~main.533c851f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fiercecontent/static/media/logo.5d5d9eef.svg"
+  }
+]);
