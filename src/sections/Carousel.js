591c6978712ab58d6f6d4d40f04faa0f1c126b37
Jim Groome <jim.groome@marketingdelivery.com>
2019-07-02 16:43:40 +0100

    Moved background images, started footer

--- src/sections/Carousel.js
+++ src/sections/Carousel.js
@@ -2,28 +2,14 @@ import React, { useEffect } from "react";
 import Slider from "react-slick";
 
 const Carousel = () => {
-  const afterCarouselLoad = () => {
-    let carouselHeight = document.getElementsByClassName("slick-slider");
-    // carouselHeight = carouselHeight[0];
-    //   .clientHeight;
-
-    console.log(carouselHeight);
-    // document.getElementsByClassName("slick-slide").style.height =
-    //   carouselHeight + "px";
-  };
-
   useEffect(() => {
     let carouselHeight = document.getElementsByClassName("slick-slider")[0]
       .clientHeight;
-    console.log(carouselHeight);
-
     var slides = document.getElementsByClassName("slick-slide");
     var i;
     for (i = 0; i < slides.length; i++) {
       slides[i].style.height = carouselHeight + "px";
     }
-
-    // slides.map(slide => ());
   }, []);
 
   const settings = {
@@ -33,7 +19,6 @@ const Carousel = () => {
     slidesToShow: 4,
     slidesToScroll: 2,
     slide: ""
-    // onInit: afterCarouselLoad()
   };
 
   const clients = [
@@ -47,9 +32,6 @@ const Carousel = () => {
     { file: "shoppertrak.png", name: "ShopperTrak" },
     { file: "autosport-international.png", name: "Autosport International" }
   ];
-
-  // Verifone, Doddle, Intel, Microsoft, Mercedes-Benz, MotorSportVision, Autosport International, SAP, ShopperTrak
-  // https://react-slick.neostack.com/docs/api/
 
   return (
     <Slider {...settings} id='logo-carousel'>
--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,15 +1,49 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+// library.add(faEnvelope, faTwitter, faLinkedin);
+
+// import {
+//   faEnvelope,
+//   faLinkedIn,
+//   faTwitter
+// } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
     <footer className='site-footer'>
       <Container fluid>
         <Row>
-          <Col>
-            <p>
-              Footer with my email address, links to Twitter and LinkedIn pages
-              and copyright details
+          <Col xs='8'>
+            <p>&copy; 2019 Fierce Content</p>
+          </Col>
+          <Col xs='4'>
+            <p className='text-right'>
+              <a
+                href='mailto:chelsea@fierce-content.com'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>{" "}
+              <a
+                href='https://uk.linkedin.com/company/fierce-content'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>{" "}
+              <a
+                href='https://twitter.com/fierce_content'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </a>
             </p>
           </Col>
         </Row>
