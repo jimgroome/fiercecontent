88126e1c4f158ad684e7c5f5ce74c9943a9ec9f7
Jim Groome <jim.groome@take1.tv>
2019-10-17 10:25:45 +0100

    Copy changes to sections 1 and 4

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -12,21 +12,29 @@ import {
 
 const Section1 = () => {
   return (
-    <div className='section section1'>
+    <div className="section section1">
       <Container>
         <Row>
-          <Col xs='12' md={{ size: 8, offset: 2 }}>
+          <Col>
+            <img
+              src={process.env.PUBLIC_URL + "/fierce-content-logo-square.png"}
+              alt="Fierce Content"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1 className="site-title">Fierce Content</h1>
+            <p className="site-tagline">
+              Strongly worded copy for your business, brand or agency
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="12" md={{ size: 8, offset: 2 }}>
             <Card>
               <CardBody>
-                <img
-                  src={
-                    process.env.PUBLIC_URL + "/fierce-content-logo-square.png"
-                  }
-                  alt='Fierce Content'
-                />
-                <CardTitle tag='h1'>Fierce Content</CardTitle>
-                <CardText tag='div'>
-                  <p>Strongly worded copy for your business, brand or agency</p>
+                <CardText tag="div">
                   <p>
                     Is your tone of voice missing true personality? Does your
                     content lack that certain sparkle? Do you simply not have
@@ -40,12 +48,38 @@ const Section1 = () => {
                     experience to your table. Let’s get creative!
                   </p>
                 </CardText>
-                <Button color='primary'>Give me a roar</Button>
+                <Button color="primary">Give me a roar</Button>
               </CardBody>
             </Card>
           </Col>
         </Row>
       </Container>
+
+      {/* <Container>
+        <Row>
+          <Col xs="12" md={{ size: 8, offset: 2 }}>
+            <Card>
+              <CardBody>
+                <CardText tag="div">
+                  <p>
+                    Is your tone of voice missing true personality? Does your
+                    content lack that certain sparkle? Do you simply not have
+                    the time to write that piece of work you so desperately
+                    need?
+                  </p>
+                  <p>Let me help.</p>
+                  <p>
+                    Whether you&#39;re a brand, a business or an overstretched
+                    agency, I’ll bring 10+ years of B2B and B2C copywriting
+                    experience to your table. Let’s get creative!
+                  </p>
+                </CardText>
+                <Button color="primary">Give me a roar</Button>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container> */}
     </div>
   );
 };
--- src/sections/Section4.js
+++ src/sections/Section4.js
@@ -12,16 +12,16 @@ import {
 
 const Section4 = () => {
   return (
-    <div className='section section4'>
+    <div className="section section4">
       <Container>
         <Row>
-          <Col xs='12' md={{ size: 8, offset: 2 }}>
+          <Col xs="12" md={{ size: 8, offset: 2 }}>
             <Card>
               <CardBody>
-                <CardTitle tag='h2' className='section-title'>
+                <CardTitle tag="h2" className="section-title">
                   What gets me fired up?
                 </CardTitle>
-                <CardText tag='div'>
+                <CardText tag="div">
                   <p>
                     Great work! Collaborating should be fun, energising and
                     inspiring. We&rsquo;re here for a good time, not a long
@@ -31,13 +31,8 @@ const Section4 = () => {
                     Let&rsquo;s put some amazing ideas together... then get home
                     to our families. It&rsquo;s all about balance.
                   </p>
-                  <p>
-                    Can we build in a function to pull through introductions to
-                    a couple of blogs here, the full versions of which will be
-                    hosted on a separate page?
-                  </p>
                 </CardText>
-                <Button color='primary'>Connect with me on LinkedIn</Button>
+                <Button color="primary">Give me a roar</Button>
               </CardBody>
             </Card>
           </Col>
